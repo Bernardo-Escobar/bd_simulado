90b2a0025d6fd1062f93f85a81c27ca71fff2984
Bernardo Victoria Escobar Silva <bernardoves@gmail.com>
2025-04-10 00:23:43 +0000

    v6

--- cliente.cpp
+++ cliente.cpp
@@ -9,27 +9,22 @@
 using namespace std;
 
 int main() {
+    Registro r;
+
     bool loop = true;
     int op = 0;
 
-    Registro r;
-
     const char* fifo_path = "fifo";
-
     int fd = open(fifo_path, O_WRONLY);
 
     do {
-        cout << "Qual operação desejada? 1-INSERT; 2-DELETE; 3-SAIR" << endl;
+        cout << "Qual operação desejada? 1-INSERT; 2-DELETE; 3-UPDATE; 4-SELECT; 5-SAIR" << endl;
         cin >> op;
         cin.ignore();
 
         switch (op){
             case 1:
                 strcpy(r.operacao, "INSERT");
-
-                cout << "Id = ";
-                cin >> r.id;
-                cin.ignore();
 
                 cout << "\nNome = ";
                 cin.getline(r.nome, 100);
@@ -38,7 +33,7 @@ int main() {
 
                 cout << "Enviado ao servidor!" << endl;
 
-                cout << r.operacao << " | " << r.id << " | " << r.nome << endl;
+                cout << r.operacao << " | " << r.nome << endl;
             break;
     
             case 2:
@@ -52,6 +47,59 @@ int main() {
             break;
 
             case 3:
+                strcpy(r.operacao, "UPDATE");
+
+                cout << "Id = ";
+                cin >> r.id;
+                cin.ignore();
+
+                cout << "Novo nome = ";
+                cin.getline(r.nome, 100);
+
+                write(fd, &r, sizeof(Registro));
+            break;
+
+            case 4: {
+                strcpy(r.operacao, "SELECT");
+
+                int opcao;
+
+                cout << "1 - Visualizar tudo\n2 - Visualizar por nome" << endl;
+                cin >> opcao;
+                cin.ignore();  // limpar o buffer
+
+                switch(opcao){
+                    case 1:
+                        r.id = -1; // ID -1 indica "mostrar tudo"
+                    break;
+
+                    case 2:
+                        r.id = -2;
+                        cout << "Nome = ";
+                        cin.getline(r.nome, 100);
+                    break;
+                }
+
+                // Envia solicitação ao servidor
+                write(fd, &r, sizeof(Registro));
+
+                // FIFO de resposta
+                const char* fifo_resposta = "fifo_resposta";
+                mkfifo(fifo_resposta, 0666);
+                int fd_resposta = open(fifo_resposta, O_RDONLY);
+
+                
+                Registro resposta;
+                while (read(fd_resposta, &resposta, sizeof(Registro)) > 0) {
+                    if (strcmp(resposta.operacao, "FIM") == 0) break;
+                    cout << resposta.id << " | " << resposta.nome << endl;
+                }
+
+                close(fd_resposta);
+            }
+            break;
+
+            case 5:
                 strcpy(r.operacao, "SAIR");
                 write(fd, &r, sizeof(Registro));
                 cout << "Comando de encerramento enviado ao servidor." << endl;
@@ -66,7 +114,4 @@ int main() {
     
 
     close(fd);
-
-    cout << r.operacao << " | " << r.id << " | " << r.nome << endl;
-    
 }
--- servidor.cpp
+++ servidor.cpp
@@ -12,16 +12,18 @@ Registro r;
 
 list<Registro> bd;
 
+int next_id = 0;
+
+const char* fifo_resposta = "fifo_resposta";
+
 pthread_mutex_t mutex_bd = PTHREAD_MUTEX_INITIALIZER;
 
 void *th_insert(void *param){
     pthread_mutex_lock(&mutex_bd); // trava
 
-    auto it = bd.begin();
-    while (it != bd.end() && it->id < r.id) {
-        ++it;
-    }
-    bd.insert(it, r);
+    r.id = next_id++;
+
+    bd.push_back(r);
 
     for (const auto& item : bd) {
         cout << item.operacao << " | " << item.id << " | " << item.nome << endl;
@@ -31,11 +33,26 @@ void *th_insert(void *param){
 	pthread_exit(0);
 }
 
-/*
 void *th_delete(void *param){
     pthread_mutex_lock(&mutex_bd);
 
+    bool encontrado = false;
 
+    // Encontra o registro com o id correspondente e remove
+    for (auto it = bd.begin(); it != bd.end(); ++it) {
+        if (it->id == r.id) {
+            bd.erase(it);
+
+            cout << "Registro com ID " << r.id << " deletado com sucesso!" << endl;
+
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Registro com ID " << r.id << " não encontrado para remoção." << endl;
+    }
 
     for (const auto& item : bd) {
         cout << item.operacao << " | " << item.id << " | " << item.nome << endl;
@@ -44,11 +61,68 @@ void *th_delete(void *param){
     pthread_mutex_unlock(&mutex_bd); // destrava antes de sair
 	pthread_exit(0);
 }
-*/
+
+void *th_update(void *param){
+    pthread_mutex_lock(&mutex_bd);
+
+    bool encontrado = false;
+
+    // Encontra o registro com o id correspondente e atualiza
+    for (auto& it : bd) {
+        if (it.id == r.id) {
+            strcpy(it.nome, r.nome); // Atualiza o nome
+
+            cout << "Registro com ID " << r.id << " atualizado com sucesso!" << endl;
+
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Registro com ID " << r.id << " não encontrado para atualização." << endl;
+    }
+
+    for (const auto& item : bd) {
+        cout << item.operacao << " | " << item.id << " | " << item.nome << endl;
+    }
+
+    pthread_mutex_unlock(&mutex_bd); // destrava antes de sair
+	pthread_exit(0);
+}
+
+void *th_select(void *param){
+    pthread_mutex_lock(&mutex_bd);
+
+    // Abrir FIFO da reposta do SELECT
+    int fd_resposta = open(fifo_resposta, O_WRONLY);
+
+    if(r.id == -1){ // Visualizar tudo
+        for(const auto& item : bd){
+            write(fd_resposta, &item, sizeof(Registro));
+        }
+    }
+    else if(r.id == -2){ // Visualizar por nome
+        for(const auto& item : bd){
+            if(strcmp(item.nome, r.nome) == 0){
+                write(fd_resposta, &item, sizeof(Registro));
+            }
+        }
+    }
+
+    // Enviar sinal de fim
+    Registro fim;
+    strcpy(fim.operacao, "FIM");
+    write(fd_resposta, &fim, sizeof(Registro));
+
+    close(fd_resposta);
+    pthread_mutex_unlock(&mutex_bd);
+	pthread_exit(0);
+}
 
 int main(){
 
-    pthread_t tid_insert, tid_delete; /* the thread identifier */
+    pthread_t tid_insert, tid_delete, tid_update, tid_select; /* the thread identifier */
 	pthread_attr_t attr; /* set of attributes for the thread */
 
     const char* fifo_path = "fifo";
@@ -70,9 +144,15 @@ int main(){
         if(strcmp(r.operacao, "INSERT") == 0){
             pthread_create(&tid_insert, &attr, th_insert, NULL);
         }
-        // else if(r.processo == "DELETE"){
-            //pthread_create(&tid_delete, &attr, th_delete, NULL);
-        // }
+        else if(strcmp(r.operacao, "DELETE") == 0){
+            pthread_create(&tid_delete, &attr, th_delete, NULL);
+        }
+        else if(strcmp(r.operacao, "UPDATE") == 0){
+            pthread_create(&tid_update, &attr, th_update, NULL);
+        }
+        else if(strcmp(r.operacao, "SELECT") == 0){
+            pthread_create(&tid_select, &attr, th_select, NULL);
+        }
         else if (strcmp(r.operacao, "SAIR") == 0) {
             cout << "Comando SAIR recebido. Encerrando servidor..." << endl;
             break;
@@ -81,4 +161,5 @@ int main(){
 
     close(fd);
     unlink(fifo_path); // remove o arquivo FIFO
+    unlink(fifo_resposta);
 }
